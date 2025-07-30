bca05bd71a4dda0ffa34f089e78587c7cc49eceb
Subhan Ahmed <subhanahmede860@gmail.com>
2025-07-30 09:25:28 +0500

    New Porftolio Design without animation Still left sections to add

--- assets/js/script.js
+++ assets/js/script.js
@@ -27,11 +27,7 @@ function animateCursor() {
 // Start animation loop
 animateCursor();
 
-
-window.onload = function () {
-
-    gsap.registerPlugin(ScrollTrigger);
-
+window.onload = () => {
     // preloader
     const preloader = document.querySelector('#preloader');
     preloader.style.opacity = '0';
@@ -39,168 +35,15 @@ window.onload = function () {
     setTimeout(() => {
         preloader.style.display = 'none';
     }, 500);
-
-    // Gsap
-    // default Slides
-    // gsap.from(".leftSlide", {
-    //     duration: 1,
-    //     opacity: 0,
-    //     x: -300,
-    //     ease: "power3.out",
-    // });
-
-    // gsap.from(".rightSlide", {
-    //     duration: 1,
-    //     opacity: 0,
-    //     x: 300,
-    //     ease: "power3.out",
-    // });
-
-    // gsap.from(".bottomSlide", {
-    //     duration: 1,
-    //     opacity: 0,
-    //     y: 200,
-    //     ease: "power3.out",
-    // });
-
-    // gsap.from(".topSlide", {
-    //     duration: 1,
-    //     opacity: 0,
-    //     y: -200,
-    //     ease: "power3.out",
-    // });
-    // End default Slides
+}
 
 
-    // Custom Slides
-    gsap.from("header .delayed", {
-        duration: 1,
-        opacity: 0,
-        y: -200,
-        delay: 2,
-        ease: "power3.out",
-    });
-
-    gsap.from(".hero h1 span", {
-        scrollTrigger: {
-            trigger: ".hero",
-            start: "top 20%",
-            end: "bottom 70%",
-            toggleActions: "play reverse restart reverse",
-        },
-        duration: 0.5,
-        opacity: 0,
-        y: -100,
-        color: "#C115B5",
-        stagger: 0.1,
-        ease: "power3.out",
-    });
-
-    gsap.from(".aboutme h2 span", {
-        scrollTrigger: {
-            trigger: ".aboutme",
-            start: "top 35%",
-            end: "bottom 0%",
-            toggleActions: "play reverse restart reverse",
-            // markers: true,
-        },
-        duration: 0.5,
-        opacity: 0,
-        y: -100,
-        color: "#C115B5",
-        stagger: 0.1,
-        ease: "power3.out",
-    });
-
-    gsap.from(".hero p", {
-        duration: 1.5,
-        opacity: 0,
-        y: 200,
-        delay: 1.2,
-        ease: "power3.out",
-    })
-
-    gsap.from(".hero h3", {
-        duration: 1.5,
-        opacity: 0,
-        y: -200,
-        delay: 1.2,
-        ease: "power3.out",
-    })
-};
-
-
-// Menu Toggle
-const menu = document.getElementById("menuToggle");
-const bars = menu.querySelectorAll(".bar");
-const sideMenuBar = document.querySelector(".menubar")
-let isOpen = false;
-
-menu.addEventListener("click", () => {
-
-    const isMobile = window.innerWidth <= 768;
-    const yOffset = isMobile ? 8.5 : 10.5;
-
-    if (!isOpen) {
-        // Open: Turn into a cross
-        gsap.to(bars[0], { rotate: 46, y: yOffset, duration: 0.3 });
-        gsap.to(bars[1], { opacity: 0, duration: 0.2 });
-        gsap.to(bars[2], { rotate: -46, y: -yOffset, duration: 0.3 });
-
-        sideMenuBar.style.transform = "translateX(0px)";
-
-        gsap.from(".myDetails .row", {
-            duration: 0.8,
-            opacity: 0,
-            x: -300,
-            ease: "power3.out",
-            delay: 1,
-        });
-
-        gsap.from(".quickLinks .row", {
-            duration: 0.8,
-            opacity: 0,
-            x: 300,
-            ease: "power3.out",
-            delay: 1,
-        });
-
-        if (!isOpen) {
-
-        }
-
+// header logo
+window.addEventListener("scroll", function () {
+    const header = document.querySelector("header");
+    if (window.scrollY > 50) {
+        header.classList.add("shrink");
     } else {
-        // Close: Turn back into bars
-        gsap.to(bars[0], { rotate: 0, y: 0, duration: 0.3 });
-        gsap.to(bars[1], { opacity: 1, duration: 0.2 });
-        gsap.to(bars[2], { rotate: 0, y: 0, duration: 0.3 });
-
-        sideMenuBar.style.transform = "translateX(-100%)";
+        header.classList.remove("shrink");
     }
-    isOpen = !isOpen;
-
-
 });
-// End Menu Toggle
-
-// site logo effect
-const siteLogo = document.querySelector("header .logo img");
-let mainHeading = document.querySelector(".hero h1");
-let mainHeadingletters = document.querySelectorAll(".hero h1 span");
-
-siteLogo.addEventListener("mouseenter", () => {
-    mainHeadingletters[0].transition = "0.3s";
-    mainHeadingletters[0].style.color = "#C115B5";
-    mainHeading.style.transition = "0.3s"
-    mainHeading.style.transform = "scale(1.1)";
-});
-
-siteLogo.addEventListener("mouseleave", () => {
-    mainHeadingletters.forEach((myHeading) => {
-        myHeading.style.color = "#FFFFFF";
-    });
-    mainHeading.style.transform = "scale(1)";
-});
-// End site logo effect
-
-
